484724de5ed3312e7680a08f50b4614f6a564fdc
susant shrestha <susantshrestha@hotmail.com>
2020-11-28 21:32:06 +0545

    interfaced

--- src/app/layout/content-layout/content-layout.module.ts
+++ src/app/layout/content-layout/content-layout.module.ts
@@ -2,18 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
-import {HeaderComponent} from '../../shared/components/header/header.component';
-import {FooterComponent} from '../../shared/components/footer/footer.component';
+import { HeaderComponent } from '../../shared/components/header/header.component';
+import { FooterComponent } from '../../shared/components/footer/footer.component';
 import { ContentLayoutComponent } from './content-layout.component';
 import { HomeComponent } from 'src/app/modules/home/home.component';
 import { ServiceComponent } from 'src/app/modules/service/service.component';
-import { ChipBoxComponent } from './../../shared/chip-box/chip-box.component';
+import { ChipBoxComponent } from './../../shared/components/chip-box/chip-box.component';
 
 
 @NgModule({
-    imports: [CommonModule,RouterModule],
-    exports: [HeaderComponent,FooterComponent],
-    declarations: [ContentLayoutComponent,ChipBoxComponent,HeaderComponent,FooterComponent,HomeComponent,ServiceComponent],
+    imports: [CommonModule, RouterModule],
+    exports: [HeaderComponent, FooterComponent],
+    declarations: [
+        ContentLayoutComponent,
+        ChipBoxComponent,
+        HeaderComponent,
+        FooterComponent,
+        HomeComponent,
+        ServiceComponent],
     providers: [],
 })
 export class ContentLayoutModule { }
--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Service } from 'src/app/data/types/service.model';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  bankingServices: Service[] = [
+    {
+      id: 1,
+      imageUrl: 'assets/img/digitalpayment.svg',
+      name: 'Digital Payment Services'
+    },
+    {
+      id: 2,
+      imageUrl: 'assets/img/chequedeposit.svg',
+      name: 'General Banking Services'
+    },
+    {
+      id: 3,
+      imageUrl: 'assets/img/cashdeposit.svg',
+      name: 'Digital Payment Services'
+    },
+    {
+      id: 4,
+      imageUrl: 'assets/img/digitalpayment.svg',
+      name: 'Card Services'
+    }
+  ];
 
   constructor() { }
 
